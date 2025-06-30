43a9678865725d30a501f5e81e3de1529975c5bb
LanSilence <642459901@qq.com>
2025-06-30 16:56:18 +0800

    使用无root方式制作镜像

---
+++ scripts/mk-ubuntu.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+set -e
+rm -rf binary
+
+# 1. 环境变量
+PYTHON_VERSION=3.13
+HASS_VERSION=2025.5.3
+FRONTEND_VERSION=20250516.0
+MATTER_SERVER_VERSION=7.0.0
+AIODISCOVER_VERSION=2.7.0
+ROOTFSIMAGE=ubuntu-24.04-rootfs.img
+ROOT_DIR=$(pwd)
+TARGET_ROOTFS_DIR=${ROOT_DIR}/binary
+SCRIPTS_DIR=$(dirname "$(readlink -f "$0")")
+HASS_SOURCE=${1:-../source/homeassistant-core/core-${HASS_VERSION}}
+IMG=homeassistant.img
+IMG_SIZE=1200M
+
+# wget http://ftp.ubuntu.com/ubuntu/pool/universe/o/opensysusers/opensysusers_0.7.3-5_all.deb -O opensysusers.deb
+
+rm -rf homeassistant.img
+
+touch  $IMG
+chmod 777 $IMG
+
+mmdebstrap --arch=arm64 \
+  --customize-hook='chroot "$1" apt install -y systemd || true ;chroot "$1" mv -f /bin/systemd-sysusers /bin/systemd-sysusers.org&& chroot "$1" ln -s /bin/echo /bin/systemd-sysusers'\
+  --customize-hook='chroot "$1" apt install -y network-manager systemd-timesyncd wpasupplicant  wireless-tools systemd-resolved u-boot-tools fdisk jq software-properties-common  openssh-server' \
+  --components="main universe multiverse restricted"\
+  --include="vim,net-tools,iproute2,curl,wget,unzip,sudo,bash,iputils-ping,libusb-1.0-0,usbutils"\
+  --setup-hook="mkdir -p \$1/etc" \
+  --customize-hook='chroot "$1" add-apt-repository -y ppa:deadsnakes/ppa'\
+  --customize-hook='chroot "$1" sed -i "/^deb .*Signed-By=/s/ Signed-By=[^ ]*//" /etc/apt/sources.list.d/deadsnakes-ubuntu-ppa-noble.sources || true' \
+  --customize-hook='chroot "$1" apt update' \
+  --customize-hook='chroot "$1"  apt install -y python3.13 python3.13-dev  python3.13-venv'\
+  --customize-hook='chroot "$1" apt install -y udev iputils-ping kmod '\
+  --customize-hook='chroot "$1" python3.13 -m ensurepip'\
+  --essential-hook='chroot "$1" mkdir -p /mnt/data'\
+  --essential-hook='chroot "$1" mkdir -p /mnt/overlay'\
+  --essential-hook='chroot "$1" mkdir -p /mnt/config'\
+  --essential-hook='chroot "$1" mkdir -p /mnt/boot'\
+  --essential-hook='chroot "$1" mkdir -p /homeassistant'\
+  --customize-hook='chroot "$1" sh -c "echo homeassistant > /etc/hostname"'\
+  --customize-hook='chroot "$1" ln -sf /usr/share/zoneinfo/Asia/Shanghai /etc/localtime'\
+  --customize-hook='chroot "$1" apt install -y rauc || true'\
+  --customize-hook='chroot "$1" systemctl mask wpa_supplicant@'\
+  --customize-hook='chroot "$1" rm /etc/apt/sources.list.d/deadsnakes-ubuntu-ppa-noble.sources'\
+  --customize-hook='cp -r hass-core/* "$1"/homeassistant/'\
+  --customize-hook='cp scripts/mmdeb-hass-img.sh "$1"/homeassistant'\
+  --customize-hook='chroot "$1" chmod +x /homeassistant/mmdeb-hass-img.sh'\
+  --customize-hook='chroot "$1" /homeassistant/mmdeb-hass-img.sh'\
+  --customize-hook='cp -rpf rootfs-overlay/* "$1"/'\
+  --customize-hook='chroot "$1" chown -R 1000:1000 /homeassistant'\
+  --customize-hook='chroot "$1" rm -rf /homeassistant/mmdeb-hass-img.sh'\
+  --customize-hook='mkfs.ext4 -b 4096 -L hass-core -d "$1"/homeassistant ./homeassistant.img 307200'\
+  --customize-hook='chroot "$1" rm -rf /homeassistant/*'\
+  --setup-hook='cp /home/lan/homeassistant/haos-core/ubuntu/rootfs-overlay/etc/shadow "$1"/etc/'\
+  --setup-hook='cp /home/lan/homeassistant/haos-core/ubuntu/rootfs-overlay/etc/passwd "$1"/etc/'\
+  --setup-hook='cp /home/lan/homeassistant/haos-core/ubuntu/rootfs-overlay/etc/group "$1"/etc/'\
+  --variant=minbase \
+  noble  rootfs.tar.gz http://ports.ubuntu.com/ubuntu-ports 
+   # --include="sudo,bash,iproute2,iputils-ping,libusb-1.0-0,usbutils,network-manager,systemd-timesyncd,wpasupplicant,unzip,wireless-tools,systemd-resolved,u-boot-tools,fdisk,jq,software-properties-common,vim,net-tools,iproute2,curl,wget,openssh-server" 
+
+mkdir binary
+tar --exclude='dev/*' -xzf rootfs.tar.gz -C binary
+
+fakeroot mkfs.erofs -zlz4hc,12  -Efragments -Ededupe -Eztailpacking ubuntu-24.04-rootfs.img binary/
